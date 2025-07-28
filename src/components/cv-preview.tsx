dd712f7968c457abdc10af9d40e33ff61ed38f2c
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-07-29 03:06:50 +0530

    Update cv-preview.tsx

--- src/components/cv-preview.tsx
+++ src/components/cv-preview.tsx
@@ -4,26 +4,21 @@ import ModernTemplate from './templates/modern-template';
 import ClassicTemplate from './templates/classic-template';
 import { Card, CardContent } from './ui/card';
 
-// Define the props interface before using it
 interface CvPreviewProps {
   cvData: CVData;
   template: string;
 }
 
-// Declare the component once
-const CvPreview = React.forwardRef<HTMLDivElement, CvPreviewProps>(({ cvData, template }, ref) => {
+// This is now a regular functional component, no longer using forwardRef.
+export default function CvPreview({ cvData, template }: CvPreviewProps) {
   return (
     <Card className="w-full print-container">
       <CardContent className="p-0">
-        <div ref={ref} className="bg-white text-black">
+        <div className="bg-white text-black">
           {template === 'modern' && <ModernTemplate cvData={cvData} />}
           {template === 'classic' && <ClassicTemplate cvData={cvData} />}
         </div>
       </CardContent>
     </Card>
   );
-});
-
-CvPreview.displayName = 'CvPreview';
-
-export default CvPreview;
+};
